5db0c096183842881235004d14c7bcde8e45de3f
HicksonWei <hanks199021@gmail.com>
2020-04-02 13:19:54 +0800

    add another source

--- index.js
+++ index.js
@@ -13,40 +13,67 @@ function fetchBody(url) {
 	return fetch(url).then(res => res.text());
 }
 
-app.get("/", (req, res) => {
-  res.send("Welcome to a basic express App");
+app.get('/', (req, res) => {
+	res.send('Welcome to a basic express App');
 });
 
 app.get('/:word', async (req, res) => {
-	const word = req.params.word;
 	try {
-		const data = await fetchBody(
-			`https://dictionary.cambridge.org/us/dictionary/english-chinese-traditional/${word}`
-		);
-		const dom = new JSDOM(data);
+		const word = req.params.word;
+		const site = req.query.q;
+		console.log(site);
+		if (site === 'oxford') {
+			const data = await fetchBody(`https://www.lexico.com/definition/${word}`);
 
-		const audio = dom.window.document.querySelectorAll('[id^=ampaudio]');
-		if ([...audio].length <= 0) {
+			const dom = new JSDOM(data);
+
+			const audio = dom.window.document.querySelectorAll('.headwordAudio');
+			if ([...audio].length <= 0) {
+				res.json({
+					status: 'fail',
+					message: 'No result!'
+				});
+				return;
+			}
+
+			const arr = [...audio].map(item => item.firstChild.src);
+			console.log(arr);
 			res.json({
-				status: 'fail',
-				message: 'No result!'
+				status: 'success',
+				message: '',
+				audioArray: arr,
+				from: 'Oxford'
 			});
-			return;
+		} else {
+			const data = await fetchBody(
+				`https://dictionary.cambridge.org/us/dictionary/english-chinese-traditional/${word}`
+			);
+			const dom = new JSDOM(data);
+
+			const audio = dom.window.document.querySelectorAll('[id^=ampaudio]');
+			if ([...audio].length <= 0) {
+				res.json({
+					status: 'fail',
+					message: 'No result!'
+				});
+				return;
+			}
+
+			let arr = [];
+			audio.forEach(item => {
+				let target = [...item.getElementsByTagName('source')].filter(
+					source => source.type === 'audio/mpeg'
+				)[0].src;
+				arr.push(target);
+			});
+			console.log(arr);
+			res.json({
+				status: 'success',
+				message: '',
+				audioArray: arr,
+				from: 'Cambridge'
+			});
 		}
-
-		let arr = [];
-		audio.forEach(item => {
-			let target = [...item.getElementsByTagName('source')].filter(
-				source => source.type === 'audio/mpeg'
-			)[0].src;
-			arr.push(target);
-		});
-
-		res.json({
-			status: 'success',
-			message: '',
-			audioArray: arr
-		});
 		/*
       [
         "/us/media/english-chinese-traditional/uk_pron/u/uka/ukalb/ukalbin030.mp3",
